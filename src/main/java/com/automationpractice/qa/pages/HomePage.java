f1d573dd00d91a62799105581ee0274c1cc4b41f
kaflemahesh <mahesh.kafle@smarsh.com>
2021-10-27 21:57:18 +0545

    Added test case for Add to cart flow.

--- src/main/java/com/automationpractice/qa/pages/HomePage.java
+++ src/main/java/com/automationpractice/qa/pages/HomePage.java
@@ -1,4 +1,40 @@
 package com.automationpractice.qa.pages;
 
-public class HomePage {
+import com.automationpractice.qa.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends TestBase {
+
+    // Initializing Page Objects using constructor
+    public HomePage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(className = "product-container")
+    WebElement image;
+
+    @FindBy(xpath = "//span[contains(text(), 'Add to cart')]")
+    WebElement AddToCartBtn;
+
+    @FindBy(xpath = " //span[contains(text(), 'There is 1 item in your cart.')]")
+    WebElement DisplayedModal;
+
+    @FindBy(className = "cross")
+    WebElement CancelBtn;
+
+    public void AddProductToCart() throws InterruptedException {
+        Actions builder = new Actions(driver);
+        builder.moveToElement(image).build().perform();
+        AddToCartBtn.click();
+        Thread.sleep(4000);
+    }
+
+    public String GetAddProductToCartMsg(){
+        String msg = DisplayedModal.getAttribute("innerText");
+        CancelBtn.click();
+        return msg;
+    }
 }
--- src/test/java/com/automationpractice/qa/testcases/HomePageTest.java
+++ src/test/java/com/automationpractice/qa/testcases/HomePageTest.java
@@ -1,4 +1,45 @@
 package com.automationpractice.qa.testcases;
 
-public class HomePageTest {
+import com.automationpractice.qa.base.TestBase;
+import com.automationpractice.qa.pages.HomePage;
+import com.automationpractice.qa.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends TestBase {
+
+    // Global Variables
+    LoginPage loginPage;
+    HomePage homePage;
+
+    public HomePageTest(){
+        // Call TestBase class constructor which initializes all properties
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        Initialization();
+        loginPage = new LoginPage();
+        homePage = new HomePage();
+        loginPage.NavigateToRegistrationPage();
+        homePage = loginPage.login();
+    }
+
+    @Test(priority = 1)
+    public void VerifyAddToCart() throws InterruptedException {
+        homePage.AddProductToCart();
+        String actualmsg = homePage.GetAddProductToCartMsg();
+       // System.out.println("------"+msg+"-------");
+        Assert.assertEquals(actualmsg, "\n" +
+                "\t\t\t\t\t\tThere is 1 item in your cart.\n" +
+                "\t\t\t\t\t");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
