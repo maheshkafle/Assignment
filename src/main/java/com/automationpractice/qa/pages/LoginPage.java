6cff32348d0fc7670537154dfe6fa888cff5bbfc
kaflemahesh <mahesh.kafle@smarsh.com>
2021-10-27 06:52:57 +0545

    Added test case for user registration flow.

--- src/main/java/com/automationpractice/qa/pages/LoginPage.java
+++ src/main/java/com/automationpractice/qa/pages/LoginPage.java
@@ -1,4 +1,109 @@
 package com.automationpractice.qa.pages;
 
-public class LoginPage {
+import com.automationpractice.qa.base.TestBase;
+import com.automationpractice.qa.utils.TestUtil;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends TestBase {
+
+    // Initializing Page Objects using constructor
+    public LoginPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    // Page Library / Object Repo
+    @FindBy(className="login")
+    WebElement SignInBtn;
+
+    @FindBy(id = "email_create")
+    WebElement EmailAddress;
+
+    @FindBy(id = "SubmitCreate")
+    WebElement CreateAccountBtn;
+
+    @FindBy(id = "customer_firstname")
+    WebElement FirstName;
+
+    @FindBy(id = "customer_lastname")
+    WebElement LastName;
+
+    @FindBy(id = "passwd")
+    WebElement Password;
+
+    @FindBy(id = "address1")
+    WebElement Address1;
+
+    @FindBy(id = "city")
+    WebElement City;
+
+    @FindBy(id = "id_state")
+    WebElement State;
+
+    @FindBy(id = "postcode")
+    WebElement PostalCode;
+
+    @FindBy(id = "id_country")
+    WebElement Country;
+
+    @FindBy(id = "phone_mobile")
+    WebElement MobilePhone;
+
+    @FindBy(id = "submitAccount")
+    WebElement SubmitAccount;
+
+    // Page Actions
+    public void NavigateToRegistrationPage(){
+        TestUtil.doClick(SignInBtn);
+    }
+
+    public void EnterEmailAddress(){
+        TestUtil.doSendKeys(EmailAddress, prop.getProperty("emailaddress"));
+    }
+
+    public void ClickOnCreateAccountBtn(){
+        TestUtil.doClick(CreateAccountBtn);
+    }
+
+    public void EnterFirstName(){
+        TestUtil.doSendKeys(FirstName, prop.getProperty("firstname"));
+    }
+
+    public void EnterLastName(){
+        TestUtil.doSendKeys(LastName, prop.getProperty("lastname"));
+    }
+
+    public void EnterPassword(){
+        TestUtil.doSendKeys(Password, prop.getProperty("password"));
+    }
+
+    public void EnterAddress1(){
+        TestUtil.doSendKeys(Address1, prop.getProperty("address1"));
+    }
+
+    public void EnterCity(){
+        TestUtil.doSendKeys(City, prop.getProperty("city"));
+    }
+
+    public void EnterState() throws InterruptedException {
+        TestUtil.selectDropdownValue(State, prop.getProperty("statevalue"));
+    }
+
+    public void EnterPostalCode(){
+        TestUtil.doSendKeys(PostalCode, prop.getProperty("postalcode"));
+    }
+
+    public void EnterCountry() throws InterruptedException {
+        TestUtil.selectDropdownValue(Country, prop.getProperty("countryvalue"));
+    }
+
+    public void EnterMobilePhone(){
+        TestUtil.doSendKeys(MobilePhone, prop.getProperty("mobilephone"));
+    }
+
+    public void ClickOnRegisterBtn(){
+        TestUtil.doClick(SubmitAccount);
+    }
+
 }
--- src/test/java/com/automationpractice/qa/testcases/LoginPageTest.java
+++ src/test/java/com/automationpractice/qa/testcases/LoginPageTest.java
@@ -1,4 +1,48 @@
 package com.automationpractice.qa.testcases;
 
-public class LoginPageTest {
+import com.automationpractice.qa.base.TestBase;
+import com.automationpractice.qa.pages.HomePage;
+import com.automationpractice.qa.pages.LoginPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends TestBase {
+
+    // Global Variables
+    LoginPage loginPage;
+    HomePage homePage;
+
+    public LoginPageTest(){
+        // Call TestBase class constructor which initializes all properties
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        Initialization();
+        loginPage = new LoginPage();
+    }
+
+    @Test(priority = 1)
+    public void RegisterUser() throws InterruptedException {
+        loginPage.NavigateToRegistrationPage();
+        loginPage.EnterEmailAddress();
+        loginPage.ClickOnCreateAccountBtn();
+        loginPage.EnterFirstName();
+        loginPage.EnterLastName();
+        loginPage.EnterPassword();
+        loginPage.EnterAddress1();
+        loginPage.EnterCity();
+        loginPage.EnterState();
+        loginPage.EnterPostalCode();
+        loginPage.EnterCountry();
+        loginPage.EnterMobilePhone();
+        loginPage.ClickOnRegisterBtn();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
