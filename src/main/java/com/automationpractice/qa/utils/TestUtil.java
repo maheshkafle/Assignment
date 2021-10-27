06cd3f2078864f73f3f115d6fa65310bdb6f02d5
kaflemahesh <mahesh.kafle@smarsh.com>
2021-10-27 06:51:45 +0545

    Added generic functions as utilities

--- src/main/java/com/automationpractice/qa/utils/TestUtil.java
+++ src/main/java/com/automationpractice/qa/utils/TestUtil.java
@@ -2,11 +2,24 @@ package com.automationpractice.qa.utils;
 
 import com.automationpractice.qa.base.TestBase;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class TestUtil extends TestBase {
 
     public static void doClick(WebElement locator){
         locator.click();
     }
+
+    public static void doSendKeys(WebElement locator, String text){
+        locator.clear();
+        locator.sendKeys(text);
+    }
+
+    public static void selectDropdownValue( WebElement locator, String value) throws InterruptedException {
+        Select dropdown_option = new Select(locator);
+        dropdown_option.selectByValue(value);
+        Thread.sleep(1000);
+    }
+
 
 }
