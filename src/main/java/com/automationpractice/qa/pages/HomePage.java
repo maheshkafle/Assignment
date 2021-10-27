57c79a9d80448182d4dc52775db4cc3b6dfecc79
kaflemahesh <mahesh.kafle@smarsh.com>
2021-10-27 22:30:59 +0545

    Added test case for product search flow

--- src/main/java/com/automationpractice/qa/pages/HomePage.java
+++ src/main/java/com/automationpractice/qa/pages/HomePage.java
@@ -1,6 +1,7 @@
 package com.automationpractice.qa.pages;
 
 import com.automationpractice.qa.base.TestBase;
+import com.automationpractice.qa.utils.TestUtil;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
@@ -25,6 +26,15 @@ public class HomePage extends TestBase {
     @FindBy(className = "cross")
     WebElement CancelBtn;
 
+    @FindBy(id = "search_query_top")
+    WebElement SearchBox;
+
+    @FindBy(className = "button-search")
+    WebElement SubmitSearchBtn;
+
+    @FindBy(className = "heading-counter")
+    WebElement DivSearchMsg;
+
     public void AddProductToCart() throws InterruptedException {
         Actions builder = new Actions(driver);
         builder.moveToElement(image).build().perform();
@@ -35,6 +45,16 @@ public class HomePage extends TestBase {
     public String GetAddProductToCartMsg(){
         String msg = DisplayedModal.getAttribute("innerText");
         CancelBtn.click();
+        return msg;
+    }
+
+    public void SearchProduct(){
+        TestUtil.doSendKeys(SearchBox, prop.getProperty("searchtext"));
+        SubmitSearchBtn.click();
+    }
+
+    public String GetSearchMsg(){
+        String msg = DivSearchMsg.getAttribute("innerText");
         return msg;
     }
 }
